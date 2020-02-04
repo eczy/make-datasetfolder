47509beeebda66f2c853d16a2161c9952efb69b4
Evan Czyzycki <eczy3826@gmail.com>
2020-02-03 20:54:46 -0800

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +8,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as fin:
 
 setup(
     name='make_datasetfolder',
-    version='0.1',
+    version='0.0.1',
     description='A utility to create a PyTorch DatasetFolder from any .csv or .tsv file with file path and class data.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -17,6 +18,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     python_requires='>=3.5',
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob('src/*.py')],
     install_requires=[],
     entry_points={
         'console_scripts': [
